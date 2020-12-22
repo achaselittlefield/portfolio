cd01e8d79f19c9d5b4480e46aa3e6a91f300f239
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-12-21 21:21:22 -0800

    Scafolding website

--- src/App.js
+++ src/App.js
@@ -3,7 +3,38 @@ import React from "react"
 function App() {
   return (
     <div>
-      App Works
+      <header>
+        <h3>Austin Littlefield</h3>
+        <nav>
+          <a>github link</a>
+          <a>Linkdeln link</a>
+          <a>Twitter link</a>
+          <a>Contact me!</a>
+          </nav>
+      </header>
+      <section>
+        <h1>Austin Littlefield</h1>
+        <h3>Full Stack Web Devolper</h3>
+        <p>About myself</p>
+        <p>Langauges & librariers I know</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>React.js</li>
+          <li>Python</li>
+        </ul>
+      <span>image over here of me doing cool stuff</span>
+      </section>
+      <section>
+        <span>Educaiton Here</span>
+        <span>Add Link to my resume</span>
+      </section>
+      <section>
+        <span>Add projects here</span>
+      </section>
+
+      <section>Dropdown menu to the side</section>
     </div>
   );
 }
